fc181e2a726e73256636f95cc86766079679c7a0
Znebur <louis.ruben@gmail.com>
2022-12-06 04:47:36 +0400

    Starting to develop the app

--- project_structure_demo/app/routes.py
+++ project_structure_demo/app/routes.py
@@ -9,8 +9,9 @@ from pyflowchart import Flowchart
 @app.route("/")
 def index():
     """index"""
-    with open(r'C:\Users\BCPL7514\DEV-ALT\developer-institute-js-python-bootcamp\week_2\day_4\follow_along\exercise_2.py') as f:
+    with open(r'/Users/rubenlouis/DEV/BOOTCAMPS/developer-institute-js-python-bootcamp/week_2/day_4/DailyChallenge/exercise_1.py') as f:
         code = f.read()
-    fc = Flowchart.from_code(code, field='Computer')
+    fc = Flowchart.from_code(code, field='')
     code_flowchart = fc.flowchart()
-    return flask.render_template("index.html", code_flowchart=code_flowchart)
+    return flask.render_template("example_flowchart_js.html", code_flowchart=code_flowchart, code=code)
+
