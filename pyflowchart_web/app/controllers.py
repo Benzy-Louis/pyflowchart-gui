092759a9728195d7c03f6ff4aac052bea1581425
Znebur <louis.ruben@gmail.com>
2022-12-09 04:14:29 +0400

    Final touch POC

--- pyflowchart_web/app/controllers.py
+++ pyflowchart_web/app/controllers.py
@@ -19,12 +19,15 @@ def content_to_flowchart(content, field) -> str:
             content, field=field)
         return flow_chart.flowchart()
     except AssertionError:
-        flow_chart = Flowchart.from_code(
-            content, field='')
-        return flow_chart.flowchart()
+        try:
+            flow_chart = Flowchart.from_code(
+                content, field='')
+            return flow_chart.flowchart()
+        except AssertionError:  # No code to parse e.g. all comments
+            return ''
 
 
-@controllers.route("/", methods=['GET'])
+@controllers.route('/', methods=['GET'])
 def index():
     """index route"""
     user_code.code = ''
@@ -32,37 +35,37 @@ def index():
     print(form_file.errors)
 
     if form_file.is_submitted():
-        print("/ submitted")
+        print('/ submitted')
 
     if form_file.validate():
-        print("/ valid")
+        print('/ valid')
 
     print(form_file.errors)
-    return flask.render_template("index.html",
-                                 title="pyflowchart GUI - Home", form_file=form_file)
+    return flask.render_template('index.html',
+                                 title='pyflowchart GUI - Home', form_file=form_file)
 
 
-@controllers.route("/", methods=['POST'])
+@controllers.route('/', methods=['POST'])
 def file_form():
     """index route"""
     form_file = FormFile()
     print(form_file.errors)
 
     if form_file.is_submitted():
-        print("/ submitted")
+        print('/ submitted')
 
     if form_file.validate():
-        print("/ valid")
+        print('/ valid')
 
     print(form_file.errors)
     if form_file.validate_on_submit():
         file = flask.request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file_path = f"{UPLOAD_FOLDER}{secure_filename(filename)}"
+            file_path = f'{UPLOAD_FOLDER}{secure_filename(filename)}'
             file.save(file_path)
 
-            with open(file_path, "r", encoding='utf-8') as f:
+            with open(file_path, 'r', encoding='utf-8') as f:
                 user_code.code = f.read()
             if os.path.exists(file_path):
                 os.remove(file_path)
@@ -70,38 +73,38 @@ def file_form():
     return flask.redirect(flask.url_for('controllers.index'))
 
 
-@ controllers.route("/parse_field", methods=['GET'])
+@ controllers.route('/parse_field', methods=['GET'])
 def field_form():
     """index route"""
     form_field = FormField()
     print(form_field.errors)
 
     if form_field.is_submitted():
-        print("/field submitted")
+        print('/field submitted')
 
     if form_field.validate():
-        print("/field valid")
+        print('/field valid')
 
     print(form_field.errors)
-    return flask.render_template("index.html",
-                                 title="pyflowchart GUI - Home", form_field=form_field, content=user_code.code)
+    return flask.render_template('index.html',
+                                 title='pyflowchart GUI - Home', form_field=form_field, content=user_code.code)
 
 
-@ controllers.route("/parse_field", methods=['POST'])
+@ controllers.route('/parse_field', methods=['POST'])
 def field_form_post():
     """index route"""
     form_field = FormField()
     print(form_field.errors)
 
     if form_field.is_submitted():
-        print("/field submitted")
+        print('/field submitted')
 
     if form_field.validate():
-        print("/field valid")
+        print('/field valid')
 
     print(form_field.errors)
 
     if form_field.validate_on_submit():
-        return flask.render_template("index.html",
-                                     title="pyflowchart GUI - Home", form_field=form_field, content=user_code.code, code_flowchart=content_to_flowchart(user_code.code, field=form_field.field.data))
+        return flask.render_template('index.html',
+                                     title='pyflowchart GUI - Home', form_field=form_field, content=user_code.code, code_flowchart=content_to_flowchart(user_code.code, field=form_field.field.data))
     return flask.redirect(flask.url_for('controllers.field_form'))
--- pyflowchart_web/wsgi.py
+++ pyflowchart_web/wsgi.py
@@ -3,5 +3,5 @@ from app import init_app
 
 app = init_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, port=8080)
