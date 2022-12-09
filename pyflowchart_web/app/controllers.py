727b96d01cef584b7db31c1d07b8c5e9e1acc09a
Znebur <louis.ruben@gmail.com>
2022-12-09 04:08:47 +0400

    Final touches for POC

--- pyflowchart_web/app/controllers.py
+++ pyflowchart_web/app/controllers.py
@@ -1,24 +1,10 @@
 """Routes and methods to manipulate requests"""
-from .models import Form, ALLOWED_EXTENSIONS, UPLOAD_FOLDER
+from .models import FormFile, FormField, ALLOWED_EXTENSIONS, UPLOAD_FOLDER, user_code
 import flask
 from pyflowchart import Flowchart
 from werkzeug.utils import secure_filename
 import os
 controllers = flask.Blueprint('controllers', __name__)
-# @controllers.route("/")
-# def index():
-#     """index"""
-#     root_path = r'C:\Users\BCPL7514\DEV-ALT\developer-institute-js-python-bootcamp'
-#     print(root_path)
-#     file_path = rf'{root_path}\week_2\day_2\ExercisesXP\exercise_4.py'
-#     print(file_path)
-#     with open(file_path, encoding='utf-8') as file:
-#         code = file.read()
-#     flow_chart = Flowchart.from_code(code, field='')
-#     code_flowchart = flow_chart.flowchart()
-#     return flask.render_template("example_flowchart_js.html",
-#                                  code_flowchart=code_flowchart,
-#                                  code=code)
 
 
 def allowed_file(filename):
@@ -26,35 +12,96 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@controllers.route("/", methods=['GET', 'POST'])
+def content_to_flowchart(content, field) -> str:
+    """Content to flowchart"""
+    try:
+        flow_chart = Flowchart.from_code(
+            content, field=field)
+        return flow_chart.flowchart()
+    except AssertionError:
+        flow_chart = Flowchart.from_code(
+            content, field='')
+        return flow_chart.flowchart()
+
+
+@controllers.route("/", methods=['GET'])
 def index():
     """index route"""
-    form = Form()
-    # if form.validate_on_submit():
-    #     file = flask.request.files['file']
-    #     if file and allowed_file(file.filename):
-    #         filename = secure_filename(file.filename)
-    #         file_path = f"{UPLOAD_FOLDER}{secure_filename(filename)}"
-    #         file.save(file_path)
+    user_code.code = ''
+    form_file = FormFile()
+    print(form_file.errors)
 
-    #         # content_stream = file.stream
-    #         # Open the file for writing.
-    #         with open(content_stream.name, 'r') as f:
-    #             # where `stuff` is, y'know... stuff to write (a string)
-    #             content = content_stream.read()
+    if form_file.is_submitted():
+        print("/ submitted")
 
-    #         # with open(file_path, "r", encoding='utf-8') as f:
-    #         #     content = f.read()
-    #         # flow_chart = Flowchart.from_code(content, field='')
-    #         # code_flowchart = flow_chart.flowchart()
-    #         code_flowchart = ''
-    #         return flask.render_template("index.html",
-    #                                      title="pyflowchart GUI", form=form,
-    #                                      content=content,
-    #                                      code_flowchart=code_flowchart)
-    #         # return flask.render_template("example_flowchart_js.html",
-    #         #                              #  title="pyflowchart GUI", form=form,
-    #         #                              code=content,
-    #         #                              code_flowchart=code_flowchart)
+    if form_file.validate():
+        print("/ valid")
+
+    print(form_file.errors)
     return flask.render_template("index.html",
-                                 title="pyflowchart GUI", form=form)
+                                 title="pyflowchart GUI - Home", form_file=form_file)
+
+
+@controllers.route("/", methods=['POST'])
+def file_form():
+    """index route"""
+    form_file = FormFile()
+    print(form_file.errors)
+
+    if form_file.is_submitted():
+        print("/ submitted")
+
+    if form_file.validate():
+        print("/ valid")
+
+    print(form_file.errors)
+    if form_file.validate_on_submit():
+        file = flask.request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = f"{UPLOAD_FOLDER}{secure_filename(filename)}"
+            file.save(file_path)
+
+            with open(file_path, "r", encoding='utf-8') as f:
+                user_code.code = f.read()
+            if os.path.exists(file_path):
+                os.remove(file_path)
+            return flask.redirect(flask.url_for('controllers.field_form'))
+    return flask.redirect(flask.url_for('controllers.index'))
+
+
+@ controllers.route("/parse_field", methods=['GET'])
+def field_form():
+    """index route"""
+    form_field = FormField()
+    print(form_field.errors)
+
+    if form_field.is_submitted():
+        print("/field submitted")
+
+    if form_field.validate():
+        print("/field valid")
+
+    print(form_field.errors)
+    return flask.render_template("index.html",
+                                 title="pyflowchart GUI - Home", form_field=form_field, content=user_code.code)
+
+
+@ controllers.route("/parse_field", methods=['POST'])
+def field_form_post():
+    """index route"""
+    form_field = FormField()
+    print(form_field.errors)
+
+    if form_field.is_submitted():
+        print("/field submitted")
+
+    if form_field.validate():
+        print("/field valid")
+
+    print(form_field.errors)
+
+    if form_field.validate_on_submit():
+        return flask.render_template("index.html",
+                                     title="pyflowchart GUI - Home", form_field=form_field, content=user_code.code, code_flowchart=content_to_flowchart(user_code.code, field=form_field.field.data))
+    return flask.redirect(flask.url_for('controllers.field_form'))
--- pyflowchart_web/app/models.py
+++ pyflowchart_web/app/models.py
@@ -6,7 +6,14 @@ UPLOAD_FOLDER = r'uploads/'
 ALLOWED_EXTENSIONS = {'py'}
 
 
-class Form(flask_wtf.FlaskForm):
+class Code():
+    """Code object to store code"""
+
+    def __init__(self, code) -> None:
+        self.code = code
+
+
+class FormFile(flask_wtf.FlaskForm):
     """Form"""
     file = wtforms.FileField('Python file to parse',
                              id='formFile',
@@ -14,9 +21,18 @@ class Form(flask_wtf.FlaskForm):
                              render_kw={'class ': 'form-control',
                                         'placeholder': 'python file to parse',
                                         'aria_label': 'python file to parse'})
+    submit = wtforms.SubmitField(
+        'Submit', render_kw={'class': 'btn btn-info'})
+
+
+class FormField(flask_wtf.FlaskForm):
     field = wtforms.StringField(
         'Field',
         render_kw={'class': 'form-control',
                             'placeholder': 'field to parse',
                             'aria_label': 'field to parse'})
-    submit = wtforms.SubmitField('Submit')
+    submit = wtforms.SubmitField(
+        'Process', render_kw={'class': 'btn btn-info'})
+
+
+user_code = Code('')
